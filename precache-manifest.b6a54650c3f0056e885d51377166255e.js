80dc4e843114471bec9703a85dfd6df09f89b467
James Carmichael <updownupdown@gmail.com>
2020-06-09 10:56:36 -0400

    Updates

---
+++ precache-manifest.b6a54650c3f0056e885d51377166255e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aacf8d9db977fcc2c6f9d23af0a5b7e4",
+    "url": "/chords/index.html"
+  },
+  {
+    "revision": "5515cc5852cadb776ba2",
+    "url": "/chords/static/css/main.f4138444.chunk.css"
+  },
+  {
+    "revision": "bdb58edd3dffe7f4f153",
+    "url": "/chords/static/js/2.9bfb2d7f.chunk.js"
+  },
+  {
+    "revision": "2f8a2ce23946d48229a1166961d4b02a",
+    "url": "/chords/static/js/2.9bfb2d7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5515cc5852cadb776ba2",
+    "url": "/chords/static/js/main.7d7940a5.chunk.js"
+  },
+  {
+    "revision": "b3d3c1b3c361d3abd1c5",
+    "url": "/chords/static/js/runtime-main.2a44c83e.js"
+  }
+]);
